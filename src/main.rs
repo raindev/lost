47f851e13e59085833c8910a40ac03f0424a9f6d
Andrew Barchuk <raindev@icloud.com>
2016-02-20 20:57:41 +0200

    Print only unsuccessful status codes and errors
    - print line numbers and links alongside errors
    - follow redirects
    - reuse curl::http::Handle among requests
    - do not stop processing on empty lines

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 extern crate curl;
 
 use curl::http;
+use curl::http::{Handle, Response};
 use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
+    let mut handle = http::handle();
+    let mut line_num = 0;
     for line in stdin.lock().lines() {
+        line_num += 1;
         let line = line.unwrap();
         if line.is_empty() {
-            return;
+            continue;
         }
-        test_url(&line);
+        url_error(&mut handle, &line)
+            .map(|error| println!("{} {} {}", line_num, line, error));
     }
 }
 
-fn test_url(url: &str) {
-    let res = http::handle()
+fn url_error(handle: &mut Handle, url: &str) -> Option<String> {
+    let result = handle
         .head(url.trim())
+        .follow_redirects(true)
         .exec();
-    match res {
-        Ok(response) => println!("Status code: {}", response.get_code()),
-        Err(err) => println!("Error: {}", err),
+    match result {
+        Ok(ref resp) if !success(&resp) => Some(resp.get_code().to_string()),
+        Err(err) => Some(err.to_string()),
+        _ => None
     }
+}
+
+fn success(response: &Response) -> bool {
+    response.get_code() >= 200 && response.get_code() < 300
 }
