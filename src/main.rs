e0fc07a249188c6faa674a903b4f68b4930652d1
Andrew Barchuk <raindev@icloud.com>
2016-03-13 14:37:36 +0200

    Implement raw prototype of recursive mode
    Implement recursive mode for URLs. Add verbose mode to enable testing.
    
    The recursive mode is very naive and inefficient. It wont do any
    tracking of already visited links and could stuck in a loop.
    
    This commit breaks piping mode.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate curl;
 extern crate regex;
+extern crate url;
 
 use curl::http;
 use curl::http::{Handle, Response};
@@ -8,17 +9,31 @@ use std::env;
 use std::io::{self, BufRead, Read, Result};
 use std::string::ToString;
 use regex::Regex;
+use url::Url;
 
 fn main() {
     let mut input = String::new();
-    let mut args = env::args();
+    let mut recursive = false;
+    let mut verbose = false;
+    let mut target = None;
+    for arg in env::args() {
+        if arg == "-R" {
+            recursive = true
+        } else if arg == "-v" {
+            verbose = true;
+        } else {
+            target = Some(arg);
+        }
+    }
     let stdin = io::stdin();
     let mut handle = http::handle();
+    let mut location = None;
     let lines =
-    if args.len() > 1 {
-        let target = args.nth(1).unwrap();
-        if target.starts_with("http") {
+    if target.is_some() {
+        let target = target.unwrap();
+        if recursive || target.starts_with("http") {
             input = url_body(&mut handle, &target);
+            location = Some(target);
             Box::new(input.lines().map(ToString::to_string)) as Box<Iterator<Item=String>>
         } else {
             File::open(target).unwrap().read_to_string(&mut input).unwrap();
@@ -27,18 +42,41 @@ fn main() {
     } else {
         Box::new(stdin.lock().lines().map(Result::unwrap)) as Box<Iterator<Item=String>>
     };
-    test_links(lines, &mut handle);
+    test_links(recursive, verbose, &location, lines, &mut handle);
 }
 
-fn test_links<'a>(lines: Box<Iterator<Item=String> + 'a>, mut handle: &mut Handle) {
+fn test_links<'a>(recursive: bool,
+                  verbose: bool,
+                  location: &Option<String>,
+                  lines: Box<Iterator<Item=String> + 'a>,
+                  mut handle: &mut Handle) {
     let mut line_num = 0;
     let url_regex = Regex::new("https?://\\w+.\\w+[^\\s'\"]+").unwrap();
     for line in lines {
         line_num += 1;
         for cap in url_regex.captures_iter(&line) {
             let url = cap.at(0).unwrap();
-            url_error(&mut handle, url)
-                .map(|error| println!("{} {} {}", line_num, url, error));
+            if verbose {
+                println!("Testing {}", url);
+            }
+            match url_error(&mut handle, url) {
+                Some(ref error) => println!("{}{} {} {}", location.clone().map_or("".to_string(), |l| l + ":"), line_num, url, error),
+                None if recursive => {
+                    let parent_url = Url::parse(&location.clone().unwrap()).unwrap();
+                    let current_url = Url::parse(url.clone()).unwrap();
+                    let parent_host = parent_url.host();
+                    let url_host = current_url.host();
+                    if parent_host == url_host {
+                        let url_body = url_body(&mut handle, &url);
+                        test_links( recursive,
+                                    verbose,
+                                    &location.clone().map(|l| l + "->" + url),
+                                    Box::new(url_body.lines().map(ToString::to_string)) as Box<Iterator<Item=String>>,
+                                    handle);
+                    }
+                },
+                _ => ()
+            }
         }
     }
 }
